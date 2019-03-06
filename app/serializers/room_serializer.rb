bba275c40e12b6143dac448f66fdec9697c93b78
Chris Camacho <cacamacho91@gmail.com>
2019-03-06 15:03:09 +0000

    serializers set up and working, room now nests all data needed

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,9 +1,12 @@
 class RoomSerializer < ActiveModel::Serializer
   has_many :users
-  attributes :id, :code, :status, :users
+  attributes :id, :code, :status, :users, :current_round
 
   class UserSerializer < ActiveModel::Serializer
-    attributes :id, :admin, :name, :score
+    attributes :id, :name, :score
   end
 
+  def current_round
+    RoundSerializer.new(object.currentRound) if object.currentRound
+  end
 end
--- app/serializers/round_serializer.rb
+++ app/serializers/round_serializer.rb
@@ -1,10 +1,4 @@
 class RoundSerializer < ActiveModel::Serializer
   belongs_to :question
-  attributes :id, :room_id, :question  
-
-  class QuestionSerializer < ActiveModel::Serializer
-    belongs_to :country
-    attributes :content, :answer, :fake_answer, :country
-  end
-
+  attributes :id, :room_id
 end
