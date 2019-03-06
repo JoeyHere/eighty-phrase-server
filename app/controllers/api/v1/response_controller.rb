60ead6203074727abda40a4002921552638d7e35
JoeyHeres <josephodell@outlook.coms>
2019-03-06 17:09:32 +0000

    responses controller

---
+++ app/controllers/api/v1/response_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::ResponsesController < ApplicationController
+
+    def create
+        @response = Response.create(response_params)
+        render json: @response
+    end
+
+    private
+
+    def response_params
+        params.require(:response).permit(:user_id,:round_id,:content)
+    end
+
+end
