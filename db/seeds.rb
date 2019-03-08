30e9ff8487d5a2248fb040c7ddc4a6cf0e85499c
JoeyHeres <josephodell@outlook.coms>
2019-03-08 14:12:38 +0000

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ Country.create([
     {name: 'Wales 🐳', img_url: ""},
     {name: 'Scotla󠁧󠁢󠁷󠁬󠁳󠁿nd 🥃', img_url: ""},
     {name: 'Italy 🇮🇹', img_url: ""},
-    {name: 'Ireland 🇮🇪', img_url: ""}
+    {name: 'Ireland 🇮🇪', img_url: ""},
+    {name: 'Australia 🇦🇺', img_url: ""}
 ])
 
 Question.create([
@@ -25,18 +26,20 @@ Question.create([
     {content: "What 5 word phrase means your talking rubbish?", answer: "your bums out the window", fake_answer: "shouting loudly at the moon", country: Country.all[2]},
     {content: "What one word refers to a person who whines or complains alot?", answer: "scunner", fake_answer: "lart", country: Country.all[2]},
 #Italy Questions
-    {content: "What phrase when translated to english, warns an opponent of an oncoming crushing defeat?", answer: "you will see green rats", fake_answer: "the pasta is dry", country: Country.all[3]},
-    {content: "What phrase when translated to english, is used to describe a perfect, good looking and kind man?", answer: "blue prince", fake_answer: "grand bravardo", country: Country.all[3]},
-    {content: "What phrase when translated to english, is used to describe being broke / out of money?", answer: "being at the green", fake_answer: "no tomatoes", country: Country.all[3]},
-    {content: "What one native word is used to refer to a state of misery / yearning?", answer: "struggimento", fake_answer: "piuttosto", country: Country.all[3]},
-    {content: "What one native word is used to refer to someone who sits at home watching tv all day?", answer: "pantofolaio", fake_answer: "moglie", country: Country.all[3]},
-    {content: "What one native word is used to refer to the watermark left by a wet glass on a table?", answer: "culaccino", fake_answer: "nessuno", country: Country.all[3]},
+    # {content: "What phrase when translated to english, warns an opponent of an oncoming crushing defeat?", answer: "you will see green rats", fake_answer: "the pasta is dry", country: Country.all[3]},
+    # {content: "What phrase when translated to english, is used to describe a perfect, good looking and kind man?", answer: "blue prince", fake_answer: "grand bravardo", country: Country.all[3]},
+    # {content: "What phrase when translated to english, is used to describe being broke / out of money?", answer: "being at the green", fake_answer: "no tomatoes", country: Country.all[3]},
+    # {content: "What one native word is used to refer to a state of misery / yearning?", answer: "struggimento", fake_answer: "piuttosto", country: Country.all[3]},
+    # {content: "What one native word is used to refer to someone who sits at home watching tv all day?", answer: "pantofolaio", fake_answer: "moglie", country: Country.all[3]},
+    # {content: "What one native word is used to refer to the watermark left by a wet glass on a table?", answer: "culaccino", fake_answer: "nessuno", country: Country.all[3]},
 #Ireland Questions
     {content: "What 3 word phrase is used to describe days getting longer?", answer: "a grand stretch", fake_answer: "my guiness overflows", country: Country.all[4]},
     {content: "What 3 word phrase is used to describe being up to no good?", answer: "acting the maggot", fake_answer: "sleeping with dragons", country: Country.all[4]},
     {content: "What 3 word phrase is used to describe the atmosphere being excelent?", answer: "craic was ninety", fake_answer: "night twas green", country: Country.all[4]},
     {content: "What 4 word phrase is used to describe going on an extended drinking session?", answer: "going on the gargle", fake_answer: "diving in the shallow", country: Country.all[4]},
     {content: "What 4 word phrase is used to describe when things are going very well?", answer: "we're sucking diesel now", fake_answer: "lifes kicked the mare", country: Country.all[4]},
+    {content: "What word would you call someone being sly?", answer: "sleeven", fake_answer: "shifthead", country: Country.all[4]},
+#Australian questions
     {content: "What word would you call someone being sly?", answer: "sleeven", fake_answer: "shifthead", country: Country.all[4]}
 #TEMPLATE (COPY FROM HERE)
     # {content: "", answer: "", fake_answer: "", country: Country.all[X]},
